ff3ad87122efe2a578919c711c846f2c5c6e8e66
Jayani Athukorala <sandunkauwu@gmail.com>
2026-05-28 16:21:33 +0200

    create order repository interface

--- src/main/java/com/example/ecommerce/repository/CategoryRepository.java
+++ src/main/java/com/example/ecommerce/repository/CategoryRepository.java
@@ -1,7 +1,6 @@
 package com.example.ecommerce.repository;
 
 import com.example.ecommerce.entity.Category;
-import com.example.ecommerce.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/example/ecommerce/repository/OrderRepository.java
+++ src/main/java/com/example/ecommerce/repository/OrderRepository.java
@@ -1,9 +1,48 @@
 package com.example.ecommerce.repository;
 
-import jakarta.persistence.criteria.Order;
+import com.example.ecommerce.entity.Order;
+import com.example.ecommerce.entity.OrderStatus;
+import com.example.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.Instant;
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    /* Required Queries */
+    // Find all orders belonging to a specific customer ID.
+    List<Order> findByCustomer_Id(Long customerId);
+
+    // Find orders by status and use a strategy to avoid the N+1 problem (loading order items in the same query).
+    @EntityGraph(attributePaths = "items")
+    List<Order> findByStatus(OrderStatus status);
+
+    /* Advanced Queries */
+    // Find orders created after a specific date.
+    List<Order> findByOrderDateAfter(Instant date);
+
+    // Find orders created between two dates.
+    List<Order> findByOrderDateBetween(Instant startDate, Instant endDate);
+
+    // Find orders that contain a specific product.
+    @Query("""
+        SELECT DISTINCT o
+        FROM Order o
+        JOIN o.items i
+        JOIN i.product p
+        WHERE p.id = :productId
+        """)
+    List<Order> findByProductId(@Param("productId") Long productId);
+
+    // Count orders by status.
+    long countByStatus(OrderStatus status);
+
+    // Find orders by customer ID and status.
+    List<Order> findByCustomer_IdAndStatus(Long customerId, OrderStatus orderStatus);
 }
