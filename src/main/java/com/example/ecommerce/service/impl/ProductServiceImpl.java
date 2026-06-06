80776fc523c3592ccdf9b50a08412628d6bbaa66
Jayani Athukorala <sandunkauwu@gmail.com>
2026-06-06 21:36:55 +0200

    completed product service

--- src/main/java/com/example/ecommerce/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/ecommerce/service/impl/ProductServiceImpl.java
@@ -2,20 +2,24 @@ package com.example.ecommerce.service.impl;
 
 import com.example.ecommerce.dto.request.ProductRequest;
 import com.example.ecommerce.dto.response.ProductResponse;
+import com.example.ecommerce.entity.Category;
+import com.example.ecommerce.entity.Product;
+import com.example.ecommerce.exception.ResourceNotFoundException;
 import com.example.ecommerce.mapper.ProductMapper;
 import com.example.ecommerce.repository.CategoryRepository;
 import com.example.ecommerce.repository.ProductRepository;
 import com.example.ecommerce.service.ProductService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 
-    ProductRepository productRepository;
-    CategoryRepository categoryRepository;
-    ProductMapper productMapper;
+    private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+    private final ProductMapper productMapper;
 
     public ProductServiceImpl(ProductRepository productRepository, CategoryRepository categoryRepository, ProductMapper productMapper){
         this.productRepository = productRepository;
@@ -24,17 +28,38 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
+    @Transactional
     public ProductResponse create(ProductRequest productRequest) {
-        return null;
+        if (productRequest == null) throw new IllegalArgumentException("Product Request cannot be empty!");
+
+        Category category = categoryRepository.findById(productRequest.categoryId())
+                .orElseThrow(() -> new ResourceNotFoundException("Category cannot found with "+ productRequest.categoryId()));
+
+        Product product = productMapper.toEntity(productRequest, category);
+        Product savedProduct = productRepository.save(product);
+
+        return productMapper.toResponse(savedProduct);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<ProductResponse> findAll() {
-        return List.of();
+
+        return productRepository.findAll()
+                .stream()
+                .map(productMapper::toResponse)
+                .toList();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<ProductResponse> searchByName(String name) {
-        return List.of();
+
+        if (name == null) throw new IllegalArgumentException("Name cannot be empty!");
+
+        return productRepository.findByNameContainingIgnoreCase(name)
+                .stream()
+                .map(productMapper::toResponse)
+                .toList();
     }
 }
