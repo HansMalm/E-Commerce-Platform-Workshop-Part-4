74194eeb0c0eae548dea3bc461cad8087484cd1a
Hans Malmefjäll <hans.malmefjall@gmail.com>
2026-06-11 13:10:22 +0200

    mod GlobalExceptionHandler.java
    add Email and Validation Exceptions

--- src/main/java/com/example/ecommerce/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/ecommerce/exception/GlobalExceptionHandler.java
@@ -3,9 +3,15 @@ package com.example.ecommerce.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentConversionNotSupportedException;
+
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -16,6 +22,40 @@ public class GlobalExceptionHandler {
         problemDetail.setTitle("Resource Not Found");
         problemDetail.setProperty("timestamp", Instant.now());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problemDetail);
+    }
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<ProblemDetail> handleEmailExists(EmailAlreadyExistsException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, ex.getMessage());
+        problemDetail.setTitle("Email Already Exists");
+        problemDetail.setProperty("timestamp", Instant.now());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(problemDetail);
+    }
+
+    @ExceptionHandler(MethodArgumentConversionNotSupportedException.class)
+    public ResponseEntity<ProblemDetail> handleValidation(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName;
+            String errorMessage = error.getDefaultMessage();
+            //Try to cast to FieldError
+            try {
+                FieldError fieldError = (FieldError) error;
+                fieldName = fieldError.getField();
+            //If it fails it is a global error
+            } catch (ClassCastException e) {
+                fieldName = error.getObjectName();
+            }
+            errors.put(fieldName, errorMessage);
+        });
+
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Validation" +
+                " Failed for one or more fields.");
+        problemDetail.setTitle("Validation Error");
+        problemDetail.setProperty("errors", errors);
+        problemDetail.setProperty("timestamp", Instant.now());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problemDetail);
+
     }
 
 }
